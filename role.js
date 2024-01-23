f65da55925317adf07acae3702e3b28b86d98e13
James_Matara <jamesmatara11@gmail.com>
2024-01-23 23:58:28 +0300

    functionality updates

--- role.js
+++ role.js
@@ -76,6 +76,13 @@ const locations = [
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters.",
   },
+
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster.",
+  },
 ];
 
 // initialize buttons
@@ -163,12 +170,20 @@ function fightDragon() {
   goFight();
 }
 
-function goFight() {}
+function goFight() {
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsters[fighting].health;
+}
 
 function attack() {
-
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText +=
+    " You attack it with your " + weapons[currentWeapon].name + ".";
+  health -= monsters[fighting].level;
+  monsterHealth -= weapons[currentWeapon].power;
 }
 
-function dodge() {
-    
-}
+function dodge() {}
